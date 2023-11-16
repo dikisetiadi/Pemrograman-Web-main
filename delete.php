05f2ce84397a9794222ca5a2dc7853b7e0d3bf2c
dikisetiadi <146998931+dikisetiadi@users.noreply.github.com>
2023-11-16 10:50:09 +0700

    Delete delete.php

--- delete.php
+++
@@ -1,9 +0,0 @@
-<?php
-require './config/db.php';
-
-if(isset($_GET['id'])) {
-    $id = $_GET['id'];
-    mysqli_query($db_connect, "DELETE FROM products WHERE id='$id'");
-    header("location: show.php");
-}
-?>
